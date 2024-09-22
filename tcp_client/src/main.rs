47dd674e4a27089177c5d527bb2043fbbffaca0b
gddeazevedo <gabrielcapuerj@gmail.com>
2024-09-22 12:57:27 -0300

    fixing codes

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -1,34 +1,32 @@
+use std::io::{self, Read, Write};
 use std::net::TcpStream;
-use std::io::{ self, Write, Read };
 use std::str;
-
 
 fn main() -> io::Result<()> {
     let mut client_socket = TcpStream::connect("127.0.0.1:12000")?;
-    let mut buffer = [0 as u8; 1024];
+    let mut buffer = [0_u8; 1024];
 
     loop {
         let message = get_input("Input lowercase sentence: ")?;
 
         if message.eq("exit") {
-            return Ok(())
+            return Ok(());
         }
 
-        client_socket.write(message.as_bytes())?;
+        client_socket.write_all(message.as_bytes())?;
 
         let bytes = client_socket.read(&mut buffer)?;
 
         match str::from_utf8(&buffer[..bytes]) {
             Ok(message) => println!("From server: {message}"),
-            Err(e)      => println!("Error: {e}")
+            Err(e) => println!("Error: {e}"),
         }
     }
 }
 
-
 fn get_input(console_message: &str) -> io::Result<String> {
     let mut input = String::new();
-    
+
     print!("{console_message}");
 
     let _ = io::stdout().flush();
